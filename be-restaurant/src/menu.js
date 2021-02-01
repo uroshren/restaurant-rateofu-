e86be180034977b8f8e2fb98d8167f12f02ed110
Uros Hren <uhren3407@gmail.com>
2021-02-01 11:08:12 +0100

    be init with schema

---
+++ be-restaurant/src/menu.js
@@ -0,0 +1,55 @@
+import gql from 'graphql-tag';
+import { buildASTSchema } from 'graphql';
+
+export const ITEMS = [
+  {
+    name: "Grilled space-whale steak with algae puree",
+    price: 66.50,
+    type: "Food"
+  },
+  {
+    name: "Tea substitute",
+    price: 1.5,
+    type: "Drink"
+  },
+  {
+    name: "Hargo biscuit",
+    price: 32,
+    type: "Food"
+  },
+  {
+    name: "Ameglian Major Cow casserole",
+    price: 55.75,
+    type: "Food"
+  },
+  {
+    name: "Pan Galactic Gargle Blaster",
+    price: 5.5,
+    type: "Drink"
+  },
+  {
+    name: "Janx Spirit",
+    price: 7,
+    type: "Drink"
+  },
+  {
+    name: "Tzjin-anthony-ks",
+    price: 11.5,
+    type: "Drink"
+  },
+];
+
+export const schema = buildASTSchema(gql`
+  type Query {
+    items: [Item]
+    item(id: ID!): Item
+  }
+
+  type Item {
+    id: ID
+    name: String
+    price: Float
+    type: String 
+  }
+`);
+
